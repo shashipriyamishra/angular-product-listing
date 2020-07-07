0add4a5a483370631b71793cb2ffdced33035149
shashi priya mishra <shashipriyamishra@gmail.com>
2020-07-07 15:54:16 +0530

    added core components header and footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,17 @@ import { HomeComponent } from './home/home/home.component';
 import { SharedModule } from "./shared/shared.module";
 import { PetComponent } from './pet/pet.component';
 import { MyOrdersComponent } from './my-orders/my-orders.component';
+import { HeaderComponent } from './core/header/header.component';
+import { FooterComponent } from './core/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     PetComponent,
-    MyOrdersComponent
+    MyOrdersComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
